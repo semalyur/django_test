789897616c4ef2b130e881015796fa455abc9863
aleksandr <semalyur200411@gmail.com>
2021-03-15 10:43:40 +0300

    Update settings.py

--- landingpageweb/settings.py
+++ landingpageweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*5rqwk3(3#1)p!cvy)=8)li6+=x-ecma8hw4ioc9+o5qbc-fn#'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
